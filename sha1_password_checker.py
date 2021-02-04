e6fc63547219ed31d3707d0e737ac71df86d8230
David Hanson <davidhanson.c@gmail.com>
2021-02-04 15:39:21 -0700

    sha1 checker update

--- sha1_password_checker.py
+++ sha1_password_checker.py
@@ -3,14 +3,30 @@ import hashlib
 import sys
 
 
+# def read_pw_file(filename):
+#     passwords = []
+#     with open(filename, 'r') as my_file:
+#         for line in my_file:
+#             temp = line.split(': ', 1)
+#             if temp[0] == 'password':
+#                 passwords.append(temp[1].strip())
+#     return passwords
+
+
 def read_pw_file(filename):
-    passwords = []
+    password_dict = dict()
     with open(filename, 'r') as my_file:
+        line_hold = ''
+        key = None
         for line in my_file:
             temp = line.split(': ', 1)
-            if temp[0] == 'password':
-                passwords.append(temp[1].strip())
-    return passwords
+            if line_hold == '':
+                key = line.strip()
+            elif temp[0] == 'password':
+                password = temp[1].strip()
+                password_dict[key] = password
+            line_hold = line.strip()
+    return password_dict
 
 
 def convert_to_hash(password):
@@ -37,7 +53,7 @@ def contact_api(first_5_hash_chars):
         raise Exception('error connecting to API')
 
 
-def check_if_pwned(password, remaining_hash, hashes_received):
+def check_if_pwned(use_name, remaining_hash, hashes_received):
     hashes_to_list = hashes_received.split()
     hash_data = []
     for hash_item in hashes_to_list:
@@ -45,15 +61,15 @@ def check_if_pwned(password, remaining_hash, hashes_received):
         hash_data.append(hash_and_count)
     for hash_item in hash_data:
         if remaining_hash.upper() == hash_item[0]:
-            return print(f"'{password}' was found in {hash_item[1]} data breeches. Change it!")
-    return print(f"'{password}' is safe. Carry on.")
+            return print(f"Password for '{use_name}' was found in {hash_item[1]} data breeches. Change it!!")
+    return print(f"Password for '{use_name}' is safe. Carry on.")
 
 
 if __name__ == '__main__':
     file = sys.argv[1]
-    password_list = read_pw_file(file)
-    for password in password_list:
+    password_dict = read_pw_file(file)
+    for key, password in password_dict.items():
         new_hash = convert_to_hash(password)
         new_hash_split = split_hash(new_hash)
         hashes_received = contact_api(new_hash_split['first 5 chars'])
-        check_if_pwned(password, new_hash_split['remaining chars'], hashes_received)
+        check_if_pwned(key, new_hash_split['remaining chars'], hashes_received)
