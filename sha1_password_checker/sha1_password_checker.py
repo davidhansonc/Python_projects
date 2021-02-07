e3a7d699b5d027e39ca0c4cbb4dfb6399cb09d1d
David Hanson <davidhanson.c@gmail.com>
2021-02-07 12:51:51 -0700

    update password checker to accept user input if no file is entered.

--- sha1_password_checker/sha1_password_checker.py
+++ sha1_password_checker/sha1_password_checker.py
@@ -1,6 +1,7 @@
 import requests
 import hashlib
 import sys
+import re
 
 
 def read_pw_file(filename):
@@ -43,23 +44,45 @@ def contact_api(first_5_hash_chars):
         raise Exception('error connecting to API')
 
 
-def check_if_pwned(use_name, remaining_hash, hashes_received):
+def check_if_pwned(remaining_hash, hashes_received, key=None):
     hashes_to_list = hashes_received.split()
     hash_data = []
     for hash_item in hashes_to_list:
         hash_and_count = hash_item.split(':')
         hash_data.append(hash_and_count)
     for hash_item in hash_data:
-        if remaining_hash.upper() == hash_item[0]:
-            return print(f"Password for '{use_name}' was found in {hash_item[1]} data breeches. Change it!!")
-    return print(f"Password for '{use_name}' is safe. Carry on.")
+        if key and remaining_hash.upper() == hash_item[0]:
+            return print(f"Password for '{key}' was found in {hash_item[1]} data breeches. Change it!!")
+        elif remaining_hash.upper() == hash_item[0]:
+            return print(f"'{password}' was found in {hash_item[1]} data breeches. Change it!!")
+    if key:
+        return print(f"Password for '{key}' is safe. Carry on.")
+    else:
+        return print(f"'{password}' is safe. Carry on.")
+
+
+def process_password(password, key=None):
+    new_hash = convert_to_hash(password)
+    new_hash_split = split_hash(new_hash)
+    hashes_received = contact_api(new_hash_split['first 5 chars'])
+    if key:
+        return check_if_pwned(new_hash_split['remaining chars'], hashes_received, key)
+    else:
+        return check_if_pwned(new_hash_split['remaining chars'], hashes_received)
 
 
 if __name__ == '__main__':
-    file = sys.argv[1]
-    password_dict = read_pw_file(file)
-    for key, password in password_dict.items():
-        new_hash = convert_to_hash(password)
-        new_hash_split = split_hash(new_hash)
-        hashes_received = contact_api(new_hash_split['first 5 chars'])
-        check_if_pwned(key, new_hash_split['remaining chars'], hashes_received)
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+        password_dict = read_pw_file(file)
+        for key, password in password_dict.items():
+            process_password(password, key)
+    else:
+        password = 'temp'
+        while password != 'q':
+            password = input("Type the password you'd like to check or type 'q' to quit: ")
+            if password == 'q':
+                print('All done!')
+                break
+            else:
+                process_password(password)
