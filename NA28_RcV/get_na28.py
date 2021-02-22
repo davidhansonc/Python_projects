2ec662bc7bd1f5bab52acc060f6b25d15d02871b
David Hanson <davidhanson.c@gmail.com>
2021-02-22 13:57:29 -0700

    retrieve rcv from lsm text-only website

--- NA28_RcV/get_na28.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.academic-bible.com/en/online-bibles/novum-testamentum-graece-na-28/read-the-bible-text/"
-r = requests.get(url)
-print(r.status_code)
-
-html_content = r.text
-soup = BeautifulSoup(html_content, 'html.parser')
